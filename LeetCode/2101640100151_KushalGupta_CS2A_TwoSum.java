38f81be30cd06dfc5cf1588af2f3daa9d2b72a56
Kushal1605 <kushalgupta1605@gmail.com>
2022-12-14 23:08:16 +0530

    Code Added.

---
+++ LeetCode/2101640100151_KushalGupta_CS2A_TwoSum.java
@@ -0,0 +1,35 @@
+/**
+ * https://leetcode.com/problems/add-two-numbers/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode newNode = new ListNode(0,null);
+        ListNode head = newNode;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0){
+            if(l1 != null){
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            newNode.next = new ListNode(carry%10);
+            carry /= 10;
+            newNode = newNode.next;
+        }
+        return head.next;
+    }
+}
