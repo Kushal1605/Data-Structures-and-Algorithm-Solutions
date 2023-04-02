26330e50941ee867e868373332553f2ef69969b8
Kushal Gupta <115387392+Kushal1605@users.noreply.github.com>
2023-04-02 11:01:11 +0530

    Create SwapNodesInPair.java

---
+++ Recursion/SwapNodesInPair.java
@@ -0,0 +1,15 @@
+/*
+*https://leetcode.com/problems/swap-nodes-in-pairs/
+*/
+
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode node = swapPairs(head.next.next);
+        head.next.next = head;
+        ListNode tempNode = head.next;
+        head.next = node;
+        return tempNode;
+    }   
+}
